45064579d3e66e72a04532d5ddef39c7c22e83ba
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-11 14:22:41 +0700

    Add dependencies and implement async main function with logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::expect_used)]
+#![warn(missing_docs)]
+
+//! TODO: Add a description
+
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize the logger
+    env_logger::init();
+
+    if let Err(err) = run().await {
+        log::error!("Error: {}", err);
+        std::process::exit(1);
+    }
+
+    Ok(())
+}
+
+async fn run() -> Result<(), Box<dyn Error>> {
+    log::info!("Starting Telemetron");
+    Ok(())
 }
