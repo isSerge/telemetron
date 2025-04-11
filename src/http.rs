f582c3305769e7c9b0cf95bda5b1793d75c14ae8
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-11 16:45:52 +0700

    Create http module

---
+++ src/http.rs
@@ -0,0 +1,70 @@
+use axum::{
+    Json, Router,
+    response::IntoResponse,
+    routing::{get, post},
+};
+use serde::Deserialize;
+use serde_json::Value;
+use tokio::net::TcpListener;
+
+use crate::error::Error;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Event {
+    source_id: u64,
+    // TODO: should be an enum
+    r#type: String,
+    // TODO: should be a timestamp
+    timestamp: String,
+    data: Option<Value>,
+}
+
+async fn ingest_handler(body: Json<Event>) -> impl IntoResponse {
+    log::info!("Ingesting data: {:?}", body);
+
+    // TODO: implement the ingestion logic
+    // TODO: validate the event
+    // TODO: store the event
+    // TODO: return a 200 response
+    // TODO: return a 400 response if the event is invalid
+    // TODO: return a 500 response if the event is valid but the ingestion fails
+
+    "Ingested data"
+}
+
+async fn stats_handler() -> impl IntoResponse {
+    log::info!("Stats");
+
+    "Stats"
+}
+
+async fn stats_by_source_id_handler() -> impl IntoResponse {
+    log::info!("Stats by source id");
+
+    "Stats by source id"
+}
+
+async fn not_found_handler() -> impl IntoResponse {
+    log::info!("Not found");
+
+    "Not found"
+}
+
+pub async fn run_server() -> Result<(), Error> {
+    log::info!("Starting Telemetron");
+
+    let routes = Router::new()
+        .route("/ingest", post(ingest_handler))
+        .route("/stats", get(stats_handler))
+        .route("/stats/{source_id}", get(stats_by_source_id_handler))
+        .fallback(not_found_handler);
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+
+    log::info!("Listening on {}", listener.local_addr()?);
+
+    axum::serve(listener, routes.into_make_service()).await?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@
 
 //! TODO: Add a description
 
+mod error;
+mod http;
+
 use std::error::Error;
 
-use axum::{
-    Json, Router,
-    response::IntoResponse,
-    routing::{get, post},
-};
-use serde::Deserialize;
-use serde_json::Value;
-use tokio::net::TcpListener;
+use http::run_server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -24,59 +20,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         log::error!("Error: {}", err);
         std::process::exit(1);
     }
-
-    Ok(())
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Event {
-    source_id: u64,
-    // TODO: should be an enum
-    r#type: String,
-    // TODO: should be a timestamp
-    timestamp: String,
-    data: Option<Value>,
-}
-
-async fn ingest_handler(body: Json<Event>) -> impl IntoResponse {
-    log::info!("Ingesting data: {:?}", body);
-
-    "Ingested data"
-}
-
-async fn stats_handler() -> impl IntoResponse {
-    log::info!("Stats");
-
-    "Stats"
-}
-
-async fn stats_by_source_id_handler() -> impl IntoResponse {
-    log::info!("Stats by source id");
-
-    "Stats by source id"
-}
-
-async fn not_found_handler() -> impl IntoResponse {
-    log::info!("Not found");
-
-    "Not found"
-}
-
-async fn run_server() -> Result<(), Box<dyn Error>> {
-    log::info!("Starting Telemetron");
-
-    let routes = Router::new()
-        .route("/ingest", post(ingest_handler))
-        .route("/stats", get(stats_handler))
-        .route("/stats/{source_id}", get(stats_by_source_id_handler))
-        .fallback(not_found_handler);
-
-    let listener = TcpListener::bind("127.0.0.1:3000").await?;
-
-    log::info!("Listening on {}", listener.local_addr()?);
-
-    axum::serve(listener, routes.into_make_service()).await?;
 
     Ok(())
 }
