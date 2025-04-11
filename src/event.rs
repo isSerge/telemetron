9849358dcf4d1ffe50178baa1eadb11c79eb0b97
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-12 04:42:48 +0700

    Refactor Event struct to use EventType enum for type field

--- src/event.rs
+++ src/event.rs
@@ -3,11 +3,16 @@ use serde::Deserialize;
 use serde_json::Value;
 
 #[derive(Debug, Deserialize)]
+pub enum EventType {
+    Heartbeat,
+}
+
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Event {
     source_id: u64,
     // TODO: should be an enum
-    r#type: String,
+    r#type: EventType,
     // TODO: should be a timestamp
     timestamp: DateTime<Utc>,
     data: Option<Value>,
