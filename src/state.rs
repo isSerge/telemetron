7c39bd339be74a6f8caaf7a9a3b372e2d3798403
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-12 07:12:47 +0700

    Add event channel to AppState

--- src/state.rs
+++ src/state.rs
@@ -1,16 +1,21 @@
 use std::sync::Arc;
 
 use dashmap::DashMap;
+use tokio::sync::mpsc;
 
 use crate::event::Event;
+
+pub type EventSender = mpsc::Sender<Event>;
+pub type EventReceiver = mpsc::Receiver<Event>;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
     storage: Arc<DashMap<u64, Event>>,
+    sender: EventSender,
 }
 
 impl AppState {
-    pub fn new() -> Self {
-        AppState { storage: Arc::new(DashMap::new()) }
+    pub fn new(sender: EventSender) -> Self {
+        AppState { storage: Arc::new(DashMap::new()), sender }
     }
 }
