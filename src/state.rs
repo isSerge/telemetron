1b45f541c764dd532ead0939179774b90f3fa240
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-13 08:03:42 +0700

    Refactor AppState to use EventsMap type

--- src/state.rs
+++ src/state.rs
@@ -1,21 +1,13 @@
-use std::sync::Arc;
-
-use dashmap::DashMap;
-use tokio::sync::mpsc;
-
-use crate::event::Event;
-
-pub type EventSender = mpsc::Sender<Event>;
-pub type EventReceiver = mpsc::Receiver<Event>;
+use crate::common_types::{EventSender, EventsMap};
 
 #[derive(Debug, Clone)]
 pub struct AppState {
-    storage: Arc<DashMap<u64, Event>>,
+    events_map: EventsMap,
     pub sender: EventSender,
 }
 
 impl AppState {
-    pub fn new(sender: EventSender) -> Self {
-        AppState { storage: Arc::new(DashMap::new()), sender }
+    pub fn new(sender: EventSender, events_map: EventsMap) -> Self {
+        AppState { events_map, sender }
     }
 }
