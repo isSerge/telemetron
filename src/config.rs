dbb3c78fdbc6f12d74742e26c6c5f48f8349c078
Serge Kovbasiuk <isserge@protonmail.com>
2025-04-16 09:19:18 +0700

    Enhance config loading and validation for plugins

--- src/config.rs
+++ src/config.rs
@@ -3,9 +3,10 @@ use std::{
     env, io,
 };
 
+use config::Environment;
 use serde::Deserialize;
 
-use crate::event::EventType;
+use crate::{event::EventType, plugins};
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct HttpConfig {
@@ -86,23 +87,81 @@ pub struct Config {
 pub enum ConfigError {
     #[error("Failed to load config file: {0}")]
     LoadError(#[from] config::ConfigError),
-
     #[error("Failed to get current working dir: {0}")]
     IoError(#[from] io::Error),
+    #[error("Unknown validation plugin(s): {0:?}")]
+    UnknownValidationPlugin(HashSet<String>),
+    #[error("Unknown processing plugin(s): {0:?}")]
+    UnknownProcessingPlugin(HashSet<String>),
 }
 
 impl Config {
+    /// Load the config file and environment variables, deserialize and validate
+    /// the config file into the Config struct
     pub fn try_load() -> Result<Self, ConfigError> {
-        let config_path = env::current_dir()?.join("config.toml");
+        let settings = Self::load_base_config()?;
 
+        let config = settings.try_deserialize::<Config>()?;
+
+        config.validate_plugins()?;
+
+        Ok(config)
+    }
+
+    /// Load the base config file and environment variables
+    fn load_base_config() -> Result<config::Config, ConfigError> {
+        let config_path = env::current_dir()?.join("config.toml");
         tracing::info!("Loading config from: {:?}", config_path);
 
-        // TODO: add source for env variables
-        let config_builder =
-            config::Config::builder().add_source(config::File::from(config_path).required(true));
+        let config_builder = config::Config::builder()
+            // Load the default config file
+            .add_source(config::File::from(config_path).required(true))
+            // Load environment variables
+            .add_source(Environment::with_prefix("TELEMETRON").prefix_separator("_").separator("_"))
+            .build()?;
 
-        let settings = config_builder.build()?;
+        Ok(config_builder)
+    }
 
-        settings.try_deserialize::<Config>().map_err(ConfigError::LoadError)
+    fn validate_plugins(&self) -> Result<(), ConfigError> {
+        // Collect known plugins from inventory
+        let known_validation_plugins: HashSet<String> =
+            inventory::iter::<plugins::ValidationPluginFactory>
+                .into_iter()
+                .map(|p| p.name.to_string())
+                .collect();
+        let known_processing_plugins: HashSet<String> =
+            inventory::iter::<plugins::ProcessingPluginFactory>
+                .into_iter()
+                .map(|p| p.name.to_string())
+                .collect();
+
+        // Check validation plugins
+        let configured_validation_plugins: HashSet<String> =
+            self.validation.plugins.keys().cloned().collect();
+        let unknown_validation_plugins = configured_validation_plugins
+            .difference(&known_validation_plugins)
+            .cloned()
+            .collect::<HashSet<_>>();
+
+        if !unknown_validation_plugins.is_empty() {
+            return Err(ConfigError::UnknownValidationPlugin(unknown_validation_plugins));
+        }
+
+        // Check processing plugins
+        let configured_processing_plugins: HashSet<String> =
+            self.processing.plugins.keys().cloned().collect();
+        let unknown_processing_plugins = configured_processing_plugins
+            .difference(&known_processing_plugins)
+            .cloned()
+            .collect::<HashSet<_>>();
+
+        if !unknown_processing_plugins.is_empty() {
+            return Err(ConfigError::UnknownProcessingPlugin(unknown_processing_plugins));
+        }
+
+        // TODO: warn about plugins that are not configured
+
+        Ok(())
     }
 }
